240cbf32fca59013891978ee23c30856fe905816
Aaron Klotnia <aklotnia@gmail.com>
2019-10-14 18:24:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,29 +6,5 @@ def nyc_pigeon_organizer(data)
         if new_array.include? item
           nil
         else
-          new_array << item
-        end
-      end
-    end
-  end
-  new_hash = {}
-  new_array.each do |item|
-    sym_item = item
-    new_hash[sym_item] = {:color => [],
-    :gender => [],
-    :lives => []}
-  end
-  data.each do |key, values|
-    values.each do |subkey, value|
-      value.each do |item|
-        if new_hash.key? item
-          new_subkey = subkey.to_s
-          new_hash[item][key] << new_subkey
-        else
-          nil
-        end
-      end
-    end
-  end
-  new_hash
+          
 end
